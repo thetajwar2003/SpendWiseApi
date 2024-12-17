5978a2e6143e3affe588eefcd9a33dfde4f4dde5
thetajwar2003 <rahmantajwar131@gmail.com>
2024-12-17 00:25:49 -0500

    dynamically change the port

--- run.py
+++ run.py
@@ -8,12 +8,16 @@ ENV = os.getenv("FLASK_ENV", "development")  # Default to "development"
 app = create_app()
 
 if __name__ == "__main__":
+    # Get PORT dynamically from environment or fallback to 5000
+    port = int(os.environ.get("PORT", 5000))
+
+    # Set the host to "0.0.0.0" for Heroku compatibility
     if ENV == "development":
         # Run the app in development mode with debugging enabled
-        app.run(host="127.0.0.1", port=5000, debug=True)
+        app.run(host="127.0.0.1", port=port, debug=True)
     elif ENV == "production":
         # Run the app in production mode
-        app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+        app.run(host="0.0.0.0", port=port)
     else:
         raise ValueError(
             "Invalid FLASK_ENV value. Must be 'development' or 'production'.")
